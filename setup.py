1e24172b88c029b05c9430ac177db9db23fd8746
Václav Klusák <vaclav.klusak@klokantech.com>
2017-10-27 16:06:57 +0200

    Release 1.7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Mesh',
-    version='1.6',
+    version='1.7',
     description='Service mesh',
     packages=['mesh'],
     install_requires=['requests>=2.12'],
