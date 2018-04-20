38a03768504f7f31633c22f16007d72738adad0d
Václav Klusák <vaclav.klusak@klokantech.com>
2018-04-20 10:01:43 +0200

    Release 2.6.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='Mesh',
-    version='2.5',
+    version='2.6',
     description='Service mesh',
     packages=['mesh'],
     extras_require={
         'amqp': ['kombu>=4.1'],
         'cron': ['schedule>=0.5'],
+        'db': ['sqlalchemy>=1.1'],
         'http': ['requests>=2.12'],
         'influx': ['influxdb>=5.0'],
         'sentry': ['raven>=6.2'],
